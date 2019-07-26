e0838f152727ea14f38eb2197b74e146c620db1f
Michael Jones <notmike@notmike.co.uk>
2019-07-26 19:11:15 +0100

    Add support for GET method

--- dns.php
+++ dns.php
@@ -11,3 +11,15 @@ if ($_SERVER['CONTENT_TYPE'] == 'application/dns-message')
                 fclose($s);
         }
 }
+else if (isset($_GET['dns']))
+{
+        $request = base64_decode(str_replace(array('-', '_'), array('+', '/'), $_GET['dns']));
+        header("Content-Type: application/dns-message");
+        $s = fsockopen("udp://127.0.0.1", 53, $errno, $errstr);
+        if ($s)
+        {
+                fwrite($s, $request);
+                echo fread($s, 4096);
+                fclose($s);
+        }
+}
